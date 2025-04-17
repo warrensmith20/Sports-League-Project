638976032a9aec3b63c5ac93286614a85681f368
warrensmith20 <letsowarren@gmail.com>
2025-04-17 12:45:55 +0200

    Added another field with getter & setter

--- Team.java
+++ Team.java
@@ -4,17 +4,20 @@ package bw.sportsleague;
 public class Team {
     private String teamName;
     private Player[] players;
+    private String stadiumName;
+
     
     
-    public Team(String teamName, Player[] players) {
+    public Team(String teamName, Player[] players,String stadiumName) {
         this.teamName = teamName;
         this.players = players;
+        this.stadiumName =stadiumName;
         
     }
 
     // Method to display team and player info
     public void displayTeamInfo() {
-        System.out.printf("Team: " + teamName);
+        System.out.printf("Team: " + teamName+"StadiumName: "+stadiumName);
         for (Player p : players) {
             p.displayPlayerInfo();
         }
@@ -24,11 +27,16 @@ public class Team {
     public String getTeamName() {
         return teamName;
     }
+    public String getStadiumName() {
+        return stadiumName;
+    }
 
     public void setTeamName(String teamName) {
         this.teamName = teamName;
     }
-
+    public void setStadiumName(String stadiumName) {
+        this.stadiumName = stadiumName;
+    }
     public Player[] getPlayers() {
         return players;
     }
